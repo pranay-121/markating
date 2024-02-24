4baaa5fb464c7dd2f83efd8ac652b14594a21d9e
pranay-121 <157474363+pranay-121@users.noreply.github.com>
2024-02-24 21:21:23 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+
+MODEL_FILE_PATH = r"/Users/pranaykumbhare/Desktop/allll/markating/linear_regression_model_on_MarketingAndSalesData.pkl"
+COLUMN_DATA_JSON = r"/Users/pranaykumbhare/Desktop/allll/markating/column_data.json"
+PORT_NUMBER = 8080
